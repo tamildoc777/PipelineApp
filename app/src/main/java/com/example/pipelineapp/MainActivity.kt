3e5eaa5e59aaaabd1c81eb7a4b4df6f0c0474a3c
tamildoc777 <121478165+tamildoc777@users.noreply.github.com>
2023-01-02 18:23:31 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/pipelineapp/MainActivity.kt
+++ app/src/main/java/com/example/pipelineapp/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.pipelineapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("test7","test7")
     }
 }
